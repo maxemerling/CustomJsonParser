b71cf855709bd00d8abb518540f5db1811edf9e6
maxemerling <maxemerling@gmail.com>
2019-09-29 01:59:01 -0700

    Finished code--no idea if it works

--- src/CustomJSONParser.java
+++ src/CustomJSONParser.java
@@ -1,30 +1,149 @@
-import java.util.HashMap;
-import java.util.Map;
+import sun.awt.X11.XSystemTrayPeer;
 
-public class CustomJSONParser extends Value {
+import javax.xml.crypto.Data;
+import java.util.*;
 
-    private static final String SEPARATOR = ",", OBJ_START = "{", OBJ_END = "}", ARR_START = "[", ARR_END = "]", STRING_MARKER = "'", VAL_MARKER = ":";
+enum VALUE {
+    OBJECT, ARRAY, NUMBER, STRING;
+}
 
-    /*
-    private Map<String, CustomJSONParser> objectMap;
-    private Map<String, String> stringMap;
-    private Map<String, Integer> intMap;
-    private Map<String, Float> floatMap;
-    private Map<String, JsonArray> arrayMap;
+public class CustomJSONParser {
+
+    /**
+     * Keys don't have quotes around them
+     * Strings are surrounded by single quotes
      */
+    public static CustomJSONParser initialize(String s) {
+        return new CustomJSONParser(s, null);
+    }
 
-    private Map<String, Value> map;
+    public String getString(String key) {
+        return data.stringMap.get(key);
+    }
 
-    private CustomJSONParser(String s) {
-        super(s);
+    public List<Object> getJSONArray(String key) {
+        return data.arrayMap.get(key);
+    }
 
-        map = new HashMap<>();
+    public float getFloat(String key) {
+        return data.floatMap.get(key);
+    }
+
+    public int getInt(String key) {
+        return data.intMap.get(key);
+    }
+
+    public String getJSONObject(String key) {
+        return data.objectMap.get(key).asString;
+    }
+
+    public List<String> listAllKeysAtCurrentLevel(int depth) {
+        List<String> keys = new ArrayList<>();
+
+        for (String key : data.allKeys) {
+            keys.add(key);
+        }
+
+        return keys;
+    }
+
+    public boolean checkIfTagExists(String tag) {
+        return data.allKeys.contains(tag);
+    }
+
+    public int maxDepth() {
+        int max = depth;
+        for (CustomJSONParser child : children) {
+            int childMaxDepth = child.maxDepth();
+            if (childMaxDepth > max) {
+                max = childMaxDepth;
+            }
+        }
+        return max;
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static final char SEPARATOR = ',', OBJ_START = '{', OBJ_END = '}', ARR_START = '[', ARR_END = ']', STRING_MARKER = '\'', VAL_MARKER = ':', FLOAT_MARKER = '.';
+
+    private DataMaps data;
+
+    private int depth;
+
+    private String asString;
+
+    private List<CustomJSONParser> children;
+
+    class DataMaps {
+        private Map<String, CustomJSONParser> objectMap;
+        private Map<String, String> stringMap;
+        private Map<String, Integer> intMap;
+        private Map<String, Float> floatMap;
+        private Map<String, List<Object>> arrayMap;
+
+        private Set<String> allKeys;
+
+        DataMaps() {
+            objectMap = new HashMap<>();
+            stringMap = new HashMap<>();
+            intMap = new HashMap<>();
+            floatMap = new HashMap<>();
+            arrayMap = new HashMap<>();
+
+            allKeys = new HashSet<>();
+        }
+
+        CustomJSONParser getObject(String key) {
+            return objectMap.get(key);
+        }
+
+        void putString(String key, String string) {
+            if (allKeys.add(key))
+                stringMap.put(key, string);
+        }
+
+        void putArr(String key, List<Object> array) {
+            if (allKeys.add(key))
+                arrayMap.put(key, array);
+        }
+
+        void putFloat(String key, Float aFloat) {
+            if (allKeys.add(key))
+                floatMap.put(key, aFloat);
+        }
+
+        void putInt(String key, Integer integer) {
+            if (allKeys.add(key))
+                intMap.put(key, integer);
+        }
+
+        void putObj(String key, CustomJSONParser object) {
+            if (allKeys.add(key))
+                objectMap.put(key, object);
+        }
+    }
+
+
+    private CustomJSONParser(String s, CustomJSONParser parent) {
+
+        if (parent == null) {
+            depth = 1;
+        } else {
+            depth = parent.depth + 1;
+            parent.children.add(this);
+        }
+
+        children = new ArrayList<>();
+
+        data = new DataMaps();
+
+        asString = s;
 
         //get to the start of the json object
         int idx = s.indexOf(OBJ_START);
 
         //loop while ______
-        while (true) {
+        do {
             //get the key by searching from the current position to the next colon
             String key = s.substring(idx + 1, s.indexOf(VAL_MARKER, idx)).trim();
 
@@ -32,22 +151,122 @@ public class CustomJSONParser extends Value {
             idx += key.length() + 1;
 
             //get to next nonwhitespace character
-            while(s.charAt(idx) == ' ') {
+
+            char currChar;
+
+            while ((currChar = s.charAt(idx)) == ' ') {
                 idx++;
             }
 
-            //test if it is an object, an array, or something else (meaning a normal value)
+            //now, idx is not on a space
 
+            switch (getType(currChar)) {
+                case STRING:
+                    data.putString(key,
+                            s.substring(idx + 1, idx = s.indexOf(STRING_MARKER, idx))
+                    );
+                    break;
+                case NUMBER:
+                    int start = idx;
+                    boolean isFloat = false;
+                    while (Character.isDigit(currChar = s.charAt(idx)) || currChar == FLOAT_MARKER ? isFloat = true : false) {
+                        idx++;
+                    }
+
+                    String number = s.substring(start, idx);
+
+                    if (isFloat) {
+                        data.putFloat(key, Float.parseFloat(number));
+                    } else {
+                        data.putInt(key, Integer.parseInt(number));
+                    }
+                    break;
+                case OBJECT:
+                    data.putObj(key, new CustomJSONParser(s.substring(idx, getObjectEnd(s, idx) + 1), this));
+                    break;
+                case ARRAY:
+                    data.putArr(key, getArray(s.substring(idx, getArrayEnd(s, idx) + 1)));
+                    break;
+            }
+
+
+            idx = s.indexOf(SEPARATOR, idx) + 1;
+            //now, idx is one space after the next comma
+
+        } while (s.indexOf(SEPARATOR, idx) > -1);
+    }
+
+    private List<Object> getArray(String s) {
+
+        List<Object> array = new ArrayList<>();
+
+        int idx = 1;
+
+        //loop while ______
+        do {
+            //get to next nonwhitespace character
+
+            char currChar;
+
+            while ((currChar = s.charAt(idx)) == ' ') {
+                idx++;
+            }
+
+            //now, idx is not on a space
+
+            switch (getType(currChar)) {
+                case STRING:
+                    array.add(s.substring(idx + 1, idx = s.indexOf(STRING_MARKER, idx)));
+                    break;
+                case NUMBER:
+                    int start = idx;
+                    boolean isFloat = false;
+                    while (Character.isDigit(currChar = s.charAt(idx)) || currChar == FLOAT_MARKER ? isFloat = true : false) {
+                        idx++;
+                    }
+
+                    String number = s.substring(start, idx);
+
+                    if (isFloat) {
+                        array.add(Float.parseFloat(number));
+                    } else {
+                        array.add(Integer.parseInt(number));
+                    }
+                    break;
+                case OBJECT:
+                    array.add(new CustomJSONParser(s.substring(idx, getObjectEnd(s, idx) + 1), this));
+                    break;
+                case ARRAY:
+                    array.add(getArray(s.substring(idx, getArrayEnd(s, idx) + 1)));
+                    break;
+            }
+
+
+            idx = s.indexOf(SEPARATOR, idx) + 1;
+            //now, idx is one space after the next comma
+
+        } while (s.indexOf(SEPARATOR, idx) > -1);
+
+        return array;
+
+    }
+
+    private static VALUE getType(char c) {
+        if (c == '\'') {
+            // the current value is a string
+            return VALUE.STRING;
+        } else if (c == ARR_START) {
+            // the current value is a json array
+            return VALUE.ARRAY;
+        } else if (c == OBJ_START) {
+            //the current value is a json object
+            return VALUE.OBJECT;
+        } else {
+            //the current value is either a string or a float
+            return VALUE.NUMBER;
         }
     }
 
-    /**
-     * Keys don't have quotes around them
-     * Strings are surrounded by single quotes
-     */
-    public static CustomJSONParser initialize(String s) {
-        return new CustomJSONParser(s);
-    }
 
     /**
      * Returns the ending index of the object in string that starts at the index "start"
@@ -60,14 +279,16 @@ public class CustomJSONParser extends Value {
         return getBoundedValue(s, start, ARR_START, ARR_END);
     }
 
-    private static int getBoundedValue(String s, int start, final String START_STRING, final String END_STRING) {
+    private static int getBoundedValue(String s, int start, final char START_STRING, final char END_STRING) {
         int closedVal = 1;
+
+        start = s.indexOf(START_STRING);
 
         while (closedVal > 0) {
             //shift start one ahead of starting brace
             start++;
             int openBrace = s.indexOf(START_STRING, start), closedBrace = s.indexOf(END_STRING, start);
-            if (openBrace < closedBrace) {
+            if (openBrace > 0 && openBrace < closedBrace) {
                 closedVal++;
                 start = openBrace;
             } else {
@@ -78,16 +299,9 @@ public class CustomJSONParser extends Value {
 
         return start;
     }
-}
 
-class Value {
-    private String rawString;
-
-    protected Value(String s) {
-        rawString = s;
+    public static void main(String[] args) {
+        String test = "{weather:  ’good’, food: 'banana', colors: [blue, yellow, red], object: {name: 'insideObj'}}";
+        System.out.println(test.substring(0, 1 + getArrayEnd(test, 0)));
     }
-}
-
-class JsonArray {
-
 }
